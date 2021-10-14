b09780e25b44e96fd07807fa8634d96c38930b7e
Coderchacha <toffees9596@gmail.com>
2021-10-15 03:39:39 +0900

    [BaekJoon]1330

---
+++ BaekJoon/1330.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync("dev/stdin")
+  .toString()
+  .trim()
+  .split(" ")
+  .map((v) => +v);
+const [a, b] = input;
+if (a < b) {
+  console.log("<");
+} else if (a > b) {
+  console.log(">");
+} else {
+  console.log("==");
+}
